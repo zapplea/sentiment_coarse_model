214a85d252e803fd64c6dd148c33f16b46785ea6
Yibing Liu <zapplea@gmail.com>
2018-02-04 22:07:30 +1100

    modify

--- sentiment/util/coarse/atr_data_generator.py
+++ sentiment/util/coarse/atr_data_generator.py
@@ -1,13 +1,33 @@
 import numpy as np
+import gensim
+from sklearn.utils import check_array
+
 class DataGenerator:
     def __init__(self,nn_config):
         self.nn_config = nn_config
         # data = [[review, is_padding, attribute labels],...]; its type is list
         self.data = np.array([1,2,3])
         np.random.shuffle(self.data)
+        self.table,self.word2id_dic=self.generate_embedding()
+
+    def generate_embedding(self):
+        """
+        generate table for look up and the dictionary used to convert words in sentence to word id.
+        :return: 
+        """
+        vecfpath = '/datastore/che313/yibing_data/nosqldb/skipgram_v200_w5'
+        word_embed = gensim.models.KeyedVectors.load_word2vec_format(vecfpath, binary=False, datatype=np.float32)
+        embed_mat = word_embed.syn0
+        vocabulary = word_embed.index2word
+        embed_mat = check_array(embed_mat, dtype='float32', order='C')
+        dictionary = {}
+        for i in range(len(vocabulary)):
+            dictionary[vocabulary[i]] = i
+
+        return dictionary, embed_mat
 
     def table_generator(self):
-        return np.random.uniform(size=(3000,200)).astype('float32')
+        return self.table
 
     def data_generator(self,mode,**kwargs):
         """
