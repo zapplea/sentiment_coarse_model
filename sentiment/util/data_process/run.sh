1943858c48a6fe5c0caf5db3a6d621c8407c18e7
Yibing Liu <zapplea@gmail.com>
2018-04-06 09:44:37 +1000

    add

--- sentiment/util/data_process/run.sh
+++ sentiment/util/data_process/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="liu121"
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=200GB
+#SBATCH --mem=5GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --qos=express
