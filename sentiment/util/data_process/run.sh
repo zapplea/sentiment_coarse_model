bafed97eea7def004ba74eb4e309d3046d15dc24
Yibing Liu <zapplea@gmail.com>
2018-04-06 09:39:43 +1000

    add

--- sentiment/util/data_process/run.sh
+++ sentiment/util/data_process/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --get-user-env
 #SBATCH --job-name="liu121"
-#SBATCH --time=24:00:00
+#SBATCH --time=01s:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
+#SBATCH --qos=express
 echo "loading"
 module load python/3.6.1
 echo "loaded"
