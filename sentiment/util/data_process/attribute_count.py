9f0a7d301025c9656a20a81b1678d582494698ed
Yibing Liu <zapplea@gmail.com>
2018-04-06 09:16:26 +1000

    add

--- sentiment/util/data_process/attribute_count.py
+++ sentiment/util/data_process/attribute_count.py
@@ -13,9 +13,9 @@ class AttributeCount:
 
 if __name__ == "__main__":
     data_config = {
-        'train_source_file_path': '/datastore/liu121/senti_data/absa_resturant_train.csv',
-        'train_data_file_path': '/datastore/liu121/senti_data/restaurant_attribute_data_train.pkl',
-        'test_source_file_path': '/datastore/liu121/senti_data/absa_resturant_test.csv',
-        'test_data_file_path': '/datastore/liu121/senti_data/restaurant_attribute_data_test.pkl',
+        'train_source_file_path': '/datastore/liu121/senti_data/semeval2016/absa_resturant_train.csv',
+        'train_data_file_path': '/datastore/liu121/senti_data/semeval2016/restaurant_attribute_data_train.pkl',
+        'test_source_file_path': '/datastore/liu121/senti_data/semeval2016/absa_resturant_test.csv',
+        'test_data_file_path': '/datastore/liu121/senti_data/semeval2016/restaurant_attribute_data_test.pkl',
     }
     ac = AttributeCount(data_config)
