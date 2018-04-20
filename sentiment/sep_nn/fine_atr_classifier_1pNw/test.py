796a56d7952f1cb32fe22119a87748ed208ef8df
Yibing Liu <zapplea@gmail.com>
2018-04-21 00:39:18 +1000

    test:sep_nn/1pNw

--- sentiment/sep_nn/fine_atr_classifier_1pNw/test.py
+++ sentiment/sep_nn/fine_atr_classifier_1pNw/test.py
@@ -8,6 +8,8 @@ else:
 from sentiment.sep_nn.fine_atr_classifier_1pNw.classifier import Classifier
 
 import unittest
+import tensorflow as tf
+import numpy as np
 
 class DataGenerator:
     def __init__(self):
@@ -23,14 +25,14 @@ class Test(unittest.TestCase):
                             'attributes_num': 13,
                             'attribute_dim': seed['word_dim'],
                             'attribute_mat_size': 3,  # number of attribute mention prototypes in a attribute matrix
-                            'words_num': 20,
+                            'words_num': 5,
                             'word_dim': seed['word_dim'],
                             'is_mat': True,
                             'epoch': 10000,
                             'batch_size': 30,
                             'lstm_cell_size': seed['lstm_cell_size'],
-                            'lookup_table_words_num': 3000000,  # 2074276 for Chinese word embedding
-                            'padding_word_index': 0,  # the index of #PAD# in word embeddings list
+                            'lookup_table_words_num': 100,  # 2074276 for Chinese word embedding
+                            'padding_word_index': 2,  # the index of #PAD# in word embeddings list
                             # flexible parameter
                             'reg_rate': 0.03,
                             'lr': 0.3,  # learing rate
@@ -50,12 +52,56 @@ class Test(unittest.TestCase):
                                 'padding_word_index': self.nn_config['padding_word_index']
                             }
         self.dg = DataGenerator()
+        self.cl = Classifier(self.nn_config, self.dg)
+        self.graph=tf.Graph()
+
+    def test_sentence_input(self):
+        X_data = np.ones(shape=(5,self.nn_config['words_num']),dtype='float32')
+        with self.graph.as_default():
+            X = self.cl.sentences_input(self.graph)
+            sess = tf.Session()
+            result = sess.run(X,feed_dict={X:X_data})
+        self.assertTrue(np.all(np.equal(result,X_data)))
+
+    def test_is_word_padding_input(self):
+        batch_size = 2
+        X_ids= np.ones(shape=(batch_size,self.nn_config['words_num']),dtype='int32')
+        for i in range(2):
+            for j in [-1,-2,-3]:
+                X_ids[i][j]=X_ids[i][j]*self.nn_config['padding_word_index']
+        with self.graph.as_default():
+            mask = self.cl.is_word_padding_input(X_ids,self.graph)
+            sess = tf.Session()
+            result = sess.run(mask)
+        self.assertEqual(result.shape,(batch_size,self.nn_config['words_num'],self.nn_config['word_dim']))
+
+        test_data = np.array([[1,1,0,0,0],[1,1,0,0,0]],dtype='float32')
+        test_data = np.tile(np.expand_dims(test_data,axis=2),[1,1,self.nn_config['word_dim']])
+        self.assertTrue(np.all(np.equal(test_data,result)))
+
+    def test_lookup_table(self):
+        batch_size = 2
+        X_ids = np.ones(shape=(batch_size, self.nn_config['words_num']), dtype='int32')
+        for i in range(2):
+            for j in [-1,-2,-3]:
+                X_ids[i][j]=X_ids[i][j]*self.nn_config['padding_word_index']
+        mask = np.array([[1, 1, 0, 0, 0], [1, 1, 0, 0, 0]], dtype='float32')
+        mask = np.tile(np.expand_dims(mask, axis=2), [1, 1, self.nn_config['word_dim']])
+
+        table_data=np.random.uniform(size=(self.nn_config['lookup_table_words_num'],self.nn_config['word_dim'])).astype('float32')
+        table_data[self.nn_config['padding_word_index']] = np.zeros(shape=(self.nn_config['word_dim'],),dtype='float32')
+        with self.graph.as_default():
+            X = self.cl.lookup_table(X_ids,mask,self.graph)
+            table = self.graph.get_collection('table')[0]
+            sess = tf.Session()
+            init=tf.global_variables_initializer()
+            sess.run(init,feed_dict={table:table_data})
+            result = sess.run(X)
+        self.assertEqual(result.shape,(batch_size,self.nn_config['words_num'],self.nn_config['word_dim']))
+        print(result)
 
     def test_classifier(self):
-        print('is_mat: ',self.nn_config['is_mat'])
-        cl = Classifier(self.nn_config,self.dg)
-        cl.classifier()
-        print('successful')
+        self.cl.classifier()
 
 if __name__ == "__main__":
     unittest.main()
