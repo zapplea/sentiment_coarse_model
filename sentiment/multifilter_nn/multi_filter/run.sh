745f0ee1ef6cc69004af0c2c40b8f780f87770bf
Yibing Liu <zapplea@gmail.com>
2018-04-20 22:26:39 +1000

    test:multifilter_nn/multi_filter

---
+++ sentiment/multifilter_nn/multi_filter/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --job-name="liu121"
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=10GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+echo "loading"
+module load python/3.6.1
+module load tensorflow/1.6.0-py36-gpu
+echo "loaded"
+
+python test.py
--- sentiment/multifilter_nn/multi_filter/test.py
+++ sentiment/multifilter_nn/multi_filter/test.py
@@ -1,5 +1,27 @@
 import unittest
+import numpy as np
+import tensorflow as tf
+
+from multi_filter import MultiFilter
 
 class Test(unittest.TestCase):
     def __init__(self):
-        nn_config
+        self.nn_config = {
+            'words_num':10,
+            'filter_size':[1,3,5]
+        }
+        self.graph = tf.Graph()
+
+
+    def test_convolution(self):
+        score = np.ones(shape=(10,6,10),dtype='float32')
+
+        with self.graph.as_default():
+            mf = MultiFilter(self.nn_config)
+            score = mf.convolution(score)
+            sess=tf.Session()
+            sess.run(score)
+            print(score)
+
+if __name__=="__main__":
+    unittest.main()
