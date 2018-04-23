3c09bfbef80046501bf6785ea3aa309925aae9b6
Yibing Liu <zapplea@gmail.com>
2018-04-23 18:14:40 +1000

    modify:name_list

--- sentiment/smartInit_nn/name_list/nostop.py
+++ sentiment/smartInit_nn/name_list/nostop.py
@@ -12,13 +12,16 @@ elif pwd.getpwuid(os.getuid()).pw_name == 'liu121':
 import csv
 namelist_filePath='/datastore/liu121/senti_data/name_list/name_list_nostop'
 files_name = os.listdir(namelist_filePath)
-dic={}
-for file_name in files_name:
-    file_path=namelist_filePath+'/'+file_name
-    dic[file_name] =[]
-    print(file_name)
-    with open(file_path,newline='') as csvfile:
-        data = csv.reader(csvfile)
-        for row in data:
-            dic[file_name].append(row[0])
-    print(dic[file_name][:10])
+
+for k in [10,15,20]:
+    dic={}
+    f=open('top'+str(k),'w')
+    for file_name in files_name:
+        file_path=namelist_filePath+'/'+file_name
+        dic[file_name] =[]
+        print(file_name)
+        with open(file_path,newline='') as csvfile:
+            data = csv.reader(csvfile)
+            for row in data:
+                dic[file_name].append(row[0])
+        f.write(dic[file_name][:k])
