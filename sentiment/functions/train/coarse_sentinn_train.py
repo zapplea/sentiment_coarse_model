26c5bd317ff1446020184f68a64c5f6f528840db
lujunyu <cs.junyu@gmail.com>
2018-05-20 16:55:45 +0800

    joint sentiment

--- sentiment/functions/train/coarse_sentinn_train.py
+++ sentiment/functions/train/coarse_sentinn_train.py
@@ -1,4 +1,159 @@
+import os
+import sys
+if os.getlogin() == 'yibing':
+    sys.path.append('/home/yibing/Documents/csiro/sentiment_coarse_model')
+elif os.getlogin() == 'lujunyu':
+    sys.path.append('/home/lujunyu/repository/sentiment_coarse_model')
+elif os.getlogin() == 'liu121':
+    sys.path.append('/home/liu121/sentiment_coarse_model')
+
+from sentiment.functions.attribute_function.metrics import Metrics
+import tensorflow as tf
+import numpy as np
+from tensorflow.python import debug as tf_debug
+
 class CoarseSentiTrain:
-    def __init__(self,nn_config,data_generator):
-        self.nn_config=nn_config
-        self.dg=data_generator
+    def __init__(self,nn_config, data_generator):
+        self.nn_config = nn_config
+        self.dg = data_generator
+        self.mt = Metrics(self.nn_config)
+
+    def train(self,classifier):
+        graph, saver = classifier
+        with graph.as_default():
+            # input
+            X = graph.get_collection('X')[0]
+            # labels
+            Y_att = graph.get_collection('Y_att')[0]
+            Y_senti = graph.get_collection('Y_senti')[0]
+            # train_step
+            train_step = graph.get_collection('opt')[0]
+            #
+            table = graph.get_collection('table')[0]
+            #
+            loss = graph.get_collection('senti_loss')[0]
+
+            pred = graph.get_collection('prediction')[0]
+
+            score = graph.get_collection('senti_score')[0]
+            TP = graph.get_collection('TP')[0]
+            FN = graph.get_collection('FN')[0]
+            FP = graph.get_collection('FP')[0]
+            keep_prob_lstm = graph.get_collection('keep_prob_lstm')[0]
+            word_score = graph.get_collection('word_score')[0]
+            check = graph.get_collection('check')[0]
+            # attribute function
+            init = tf.global_variables_initializer()
+        max_f1_score = 0
+        table_data = self.dg.table
+        print(self.dg.aspect_dic)
+
+        with graph.device('/gpu:1'):
+            config = tf.ConfigProto(allow_soft_placement=True)
+            config.gpu_options.allow_growth = True
+            with tf.Session(graph=graph, config=config) as sess:
+                sess.run(init, feed_dict={table: table_data})
+                # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+
+                batch_num = int(self.dg.train_data_size / self.nn_config['batch_size'])
+                print('Train set size: ', self.dg.train_data_size, 'validation set size:', self.dg.val_data_size)
+                for i in range(self.nn_config['epoch']):
+                    loss_vec = []
+                    TP_vec = []
+                    FP_vec = []
+                    FN_vec = []
+                    for j in range(batch_num):
+                        sentences, Y_att_data ,Y_senti_data= self.dg.data_generator(j,'train')
+                        _, train_loss, TP_data, FP_data, FN_data, pred_data, score_data ,check_data,word_score_data\
+                            = sess.run(
+                            [train_step, loss, TP, FP, FN, pred, score ,check,word_score],
+                            feed_dict={X: sentences, Y_att: Y_att_data,Y_senti:Y_senti_data,
+                                       keep_prob_lstm: self.nn_config['keep_prob_lstm']})
+
+                        ###Show training message
+                        # print(j,train_loss)
+                        # print(check_data)
+                        # print('Batch :',j,'Training loss:%0.8f'%train_loss)
+                        # #
+                        random_display = np.random.randint(0, self.nn_config['batch_size'])
+                        pred_check = [(list(self.dg.aspect_dic.keys())[rrr],list(self.dg.senti_dic.keys())[c]) for rrr in range(self.nn_config['attributes_num']) for c, rr in enumerate(pred_data[random_display][rrr]) if rr]
+                        Y_att_check = [(list(self.dg.aspect_dic.keys())[rrr],list(self.dg.senti_dic.keys())[c]) for rrr in range(self.nn_config['attributes_num']) for c, rr in enumerate(Y_senti_data[random_display][rrr]) if rr]
+                        sentences_check = [list(self.dg.dictionary.keys())[word] for word in sentences[random_display] if word]
+                        senti_score_check = word_score_data[random_display]
+                        print("sentence id: ", random_display, "\nsentence:\n", sentences_check,"\nreview length:\n", len(sentences_check), "\npred:\n",pred_check,"\nY_att:\n", Y_att_check,'\nsentiment score:',word_score_data)
+                        #
+                        loss_vec.append(train_loss)
+                        TP_vec.append(TP_data)
+                        FP_vec.append(FP_data)
+                        FN_vec.append(FN_data)
+
+                    print('Epoch:', i, '\nTraining loss:%.10f' % np.mean(loss_vec))
+                    if i % 1 == 0:
+                        _precision = self.mt.precision(TP_vec,FP_vec,'macro')
+                        _recall = self.mt.recall(TP_vec,FN_vec,'macro')
+                        _f1_score = self.mt.f1_score(_precision,_recall,'macro')
+                        print('F1 score for each class:',_f1_score,'\nPrecision for each class:',_precision,'\nRecall for each class:',_recall)
+                        print('Macro F1 score:',np.mean(_f1_score) ,' Macro precision:', np.mean(_precision),' Macro recall:', np.mean(_recall) )
+
+                        _precision = self.mt.precision(TP_vec, FP_vec, 'micro')
+                        _recall = self.mt.recall(TP_vec, FN_vec, 'micro')
+                        _f1_score = self.mt.f1_score(_precision, _recall, 'micro')
+                        print('Micro F1 score:', _f1_score, ' Micro precision:', np.mean(_precision), ' Micro recall:', np.mean(_recall))
+
+
+                    if i % 1 == 0 and i != 0:
+                        loss_vec = []
+                        pred_vec = []
+                        score_vec = []
+                        score_pre_vec = []
+                        Y_att_vec = []
+                        TP_vec = []
+                        FP_vec = []
+                        FN_vec = []
+                        val_batch_num = int(self.dg.val_data_size / self.nn_config['batch_size'])
+                        for j in range(val_batch_num):
+                            sentences, Y_att_data,Y_senti_data = self.dg.data_generator(j,'val')
+                            val_loss, pred_data, score_data, TP_data, FP_data, FN_data = sess.run(
+                                [loss, pred, score, TP, FP, FN],
+                                feed_dict={X: sentences,
+                                           Y_att: Y_att_data,
+                                           Y_senti: Y_senti_data,
+                                           keep_prob_lstm: 1.0
+                                           })
+                            ##Show test message
+                            random_display = np.random.randint(0, self.nn_config['batch_size'])
+                            # if random_display % 6 == 0:
+                            #     display_start = random_display * self.nn_config['max_review_length']
+                            #     display_end = (random_display+1) * self.nn_config['max_review_length']
+                            #     pred_check = [list(self.dg.aspect_dic.keys())[c] for c, rr in enumerate(np.sum(pred_data[display_start:display_end],axis=0)) if rr]
+                            #     Y_att_check = [list(self.dg.aspect_dic.keys())[c] for c, rr in enumerate(np.sum(true_labels_data[display_start:display_end],axis=0)) if rr]
+                            #     sentences_check = [[list(self.dg.dictionary.keys())[word] for word in s if word != self.nn_config['padding_word_index']] for s in sentences[random_display] if [list(self.dg.dictionary.keys())[word] for word in s if word != self.nn_config['padding_word_index']]]
+                            #     coarse_atr_score_check = score_data[display_start:display_end][range(len(sentences_check))]
+                            #     print('\nBatch:',j,"\nsentence id: ", random_display, "\nsentence:\n", sentences_check,"\nreview length:\n", len(sentences_check), "\npred:\n",pred_check,"\nY_att:\n", Y_att_check,'\ncoarse score:',coarse_atr_score_check)
+                            TP_vec.append(TP_data)
+                            FP_vec.append(FP_data)
+                            FN_vec.append(FN_data)
+                            loss_vec.append(val_loss)
+                            for n in range(self.nn_config['batch_size']):
+                                pred_vec.append(pred_data[n])
+                                score_vec.append(score_data[n])
+                        print('\nVal_loss:%.10f' % np.mean(loss_vec))
+
+                        _precision = self.mt.precision(TP_vec, FP_vec, 'macro')
+                        _recall = self.mt.recall(TP_vec, FN_vec, 'macro')
+                        _f1_score = self.mt.f1_score(_precision, _recall, 'macro')
+                        print('F1 score for each class:', _f1_score, '\nPrecision for each class:', _precision,
+                              '\nRecall for each class:', _recall)
+                        print('Macro F1 score:', np.mean(_f1_score), ' Macro precision:', np.mean(_precision),
+                              ' Macro recall:', np.mean(_recall))
+
+                        _precision = self.mt.precision(TP_vec, FP_vec, 'micro')
+                        _recall = self.mt.recall(TP_vec, FN_vec, 'micro')
+                        _f1_score = self.mt.f1_score(_precision, _recall, 'micro')
+                        print('Micro F1 score:', _f1_score, ' Micro precision:', np.mean(_precision),
+                              ' Micro recall:', np.mean(_recall))
+
+                        if max_f1_score < _f1_score:
+                            max_f1_score = _f1_score
+                            # saver.save(sess,self.nn_config['model_save_path'],global_step=i+1)
+                        print('Max Micro F1 score: ',max_f1_score)
---
+++ sentiment/joint_nn/joint_1pNw_rd/learn.py
@@ -0,0 +1,77 @@
+import os
+import sys
+if os.getlogin() == 'yibing':
+    sys.path.append('/home/yibing/Documents/csiro/sentiment_coarse_model')
+elif os.getlogin() == 'lujunyu':
+    sys.path.append('/home/lujunyu/repository/sentiment_coarse_model')
+elif os.getlogin() == 'liu121':
+    sys.path.append('/home/liu121/sentiment_coarse_model')
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2" ## 0
+
+from sentiment.joint_nn.joint_1pNw_rd.classifier import Classifier
+from sentiment.util.fine.senti_data_generator import DataGenerator
+# from sentiment.util.fine.senti_data_generator import DataGenerator
+
+def main(nn_config,data_config):
+    # dg = DataGenerator_random(data_config)
+    dg = DataGenerator(data_config,nn_configs)
+    cl = Classifier(nn_config,dg)
+    cl.train()
+
+
+if __name__ == "__main__":
+
+    seed = {'lstm_cell_size': 300,
+            'word_dim': 300
+            }
+    nn_configs = { # fixed parameter
+        'attributes_num': 12,
+        'attribute_dim': seed['word_dim'],
+        'attribute_mat_size': 3,  # number of attribute mention prototypes in a attribute matrix
+        'words_num': 40,
+        'word_dim': seed['word_dim'],
+        'is_mat': True,
+        'epoch': 1000,
+        'batch_size': 10,
+        'lstm_cell_size': seed['lstm_cell_size'],
+        'lookup_table_words_num': 30342,  # 2074276 for Chinese word embedding
+        'padding_word_index': 30341,  # the index of #PAD# in word embeddings list
+        # flexible parameter
+        'reg_rate': 0.003,
+        'lr': 0.00001,  # learing rate
+        'senti_lr': 0.00001,
+        'joint_lr':0.0003,
+        'atr_pred_threshold': 0,
+        # if score of attribute is larger than atr_pred_threshold, the attribute exists in the sentence
+        'attribute_loss_theta': 3.0,
+        'keep_prob_lstm': 0.5,
+        'attribute_senti_prototype_num': 4, #10  number of sentiment prototypes for a specific attribute
+        'normal_senti_prototype_num': 4, #10 number of sentiment prototypes for each normal sentiment polarity like "Negative"
+        'sentiment_dim': seed['lstm_cell_size'], # dim of a sentiment expression prototype.
+        'rel_words_num':20, # number of dependency-parser relation words
+        'rel_word_dim':seed['word_dim'], # a relation word's dimension
+        'sentiment_loss_theta': 1.0, # deprecated
+        'rps_num': 5, # number of relative distance. if it is 5, then it means , for word_i, the distance between the other words and word_i is at most 5. if the distance is greater than 5, then we still consider the distance as 5.
+        'rp_dim': seed['lstm_cell_size'], # dimension of relative distance embedding
+        'atr_threshold': 0, # attribute score threshold, not used in here
+        'senti_pred_threshold':0, # deprecated, used to predict sentiment
+        'max_path_length':20 # the maximal length of dependency path, the util/dependency_parser/dependency_generator.py will print it out.
+                 }
+    data_config = {
+        'train_source_file_path': '/home/lujunyu/dataset/semeval2016/absa_resturant_train.pkl',
+        'train_data_file_path': '/home/lujunyu/repository/sentiment_coarse_model/sentiment/senti_nn/train_data.pkl',
+        'test_source_file_path': '/home/lujunyu/dataset/semeval2016/absa_resturant_test.pkl',
+        'test_data_file_path': '/home/lujunyu/repository/sentiment_coarse_model/sentiment/senti_nn/test_data.pkl',
+        'wordembedding_file_path': '~/dataset/word2vec-GoogleNews-vectors/GoogleNews-vectors-negative300.bin',
+        'stopwords_file_path': '~/dataset/semeval2016/stopwords.txt',
+        'attributes_num': nn_configs['attributes_num'],
+        'batch_size': nn_configs['batch_size'],
+        'words_num': nn_configs['words_num'],
+        'padding_word_index': nn_configs['padding_word_index'],
+        'word_dim': seed['word_dim'],
+        'top_k_data': -1,
+        'dictionary': '/home/lujunyu/repository/sentiment_coarse_model/sentiment/util/word_dic/data_dictionary.pkl'
+    }
+    main(nn_configs,data_config)
