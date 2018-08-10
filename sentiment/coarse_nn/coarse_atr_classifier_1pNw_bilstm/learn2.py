18d6388c61d2456683e78a8bc83209ea06bce081
Yibing Liu <zapplea@gmail.com>
2018-08-11 01:54:35 +1000

    add

--- sentiment/coarse_nn/coarse_atr_classifier_1pNw_bilstm/learn2.py
+++ sentiment/coarse_nn/coarse_atr_classifier_1pNw_bilstm/learn2.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
             'attribute_mat_size': 5,
             'attributes_num': 6,
             }
-    reg_rate = [3E-4, ]
-    lr = [3E-3, ]
+    reg_rate = [3E-4, 3E-3, 3E-2, 3E-1]
+    lr = [3E-3, 3E-3, 3E-3, 3E-3]
     nn_config = {
         # fixed parameter
         'attributes_num': seed['attributes_num'],
