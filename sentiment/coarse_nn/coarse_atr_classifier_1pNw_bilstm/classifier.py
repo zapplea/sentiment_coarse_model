8a090557d228d8efebebf3b4058bb239a843362a
Yibing Liu <zapplea@gmail.com>
2018-08-14 21:59:45 +1000

    modify:/coarse_nn/relevance_score:loss function: change loss function to eliminate influence of padded sentences

--- sentiment/coarse_nn/coarse_atr_classifier_1pNw_bilstm/classifier.py
+++ sentiment/coarse_nn/coarse_atr_classifier_1pNw_bilstm/classifier.py
@@ -78,14 +78,16 @@ class Classifier:
             # eliminate the influce of -inf when calculate relevance probability with softmax
             condition = tf.is_inf(score)
             score = tf.where(condition, tf.zeros_like(score), score)
+            # the mask is used to eliminate the influence of padded sentences when calculate p(x|a) and loss
+            mask = tf.where(condition, tf.zeros_like(score), tf.ones_like(score))
             # aspect_prob.shape = (batch size * max review length ,attributes num)
             aspect_prob = relscore.expand_aspect_prob(aspect_prob, graph)
             # atr_rel_prob = (batch size * max review length, attributes num)
-            atr_rel_prob = relscore.relevance_prob_atr(score, graph)
+            atr_rel_prob = relscore.relevance_prob_atr(score, mask, graph)
             # coarse score
             # score = relscore.coarse_atr_score(aspect_prob=aspect_prob, rel_prob=atr_rel_prob, atr_score=score)
             # loss.shape=(batch_size*max review length, attributes num)
-            loss = relscore.sigmoid_loss(score, Y_att, atr_rel_prob, aspect_prob, graph)
+            loss = relscore.sigmoid_loss(score, Y_att, atr_rel_prob, aspect_prob, mask, graph)
             #loss = tf.multiply(atr_rel_prob,tf.multiply(aspect_prob,loss))
             tf.add_to_collection('coarse_atr_loss',loss)
             pred = self.af.prediction(score, graph)
