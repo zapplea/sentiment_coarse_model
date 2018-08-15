d6aae67eaa857241d4534de76c8879282796b4d5
Yibing Liu <zapplea@gmail.com>
2018-08-16 05:29:47 +1000

    add

--- sentiment/analysis/ana_ilp_1pNw.py
+++ sentiment/analysis/ana_ilp_1pNw.py
@@ -49,7 +49,7 @@ class Analysis:
             self.fine_table = pickle.load(f)
         self.id2word={}
         for word in self.fine_word_dic:
-            self.id2word[word]=self.fine_word_dic[word]
+            self.id2word[self.fine_word_dic[word]]=word
         print('#PAD#: ',self.fine_word_dic['#PAD#'])
         print(len(self.id2word))
         print(self.id2word[34933])
