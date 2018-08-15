e7ad7b953e6788cd7d8bee2f67b212a3df6482e4
Yibing Liu <zapplea@gmail.com>
2018-08-16 02:41:47 +1000

    add

--- sentiment/analysis/ana_ilp_1pNw.py
+++ sentiment/analysis/ana_ilp_1pNw.py
@@ -127,7 +127,7 @@ def main(coarse_nn_config, fine_nn_config, coarse_data_config, fine_data_config)
     ana = Analysis(coarse_nn_config, fine_nn_config, coarse_data_config, fine_data_config, config_ana)
     init_data = ana.transfer_data_generator()
     ana.aspect_mention_vector_nearest_word(init_data)
-    ana.attribute_mention_vector_nearest_word(init_data)
+    # ana.attribute_mention_vector_nearest_word(init_data)
 
 if __name__ =="__main__":
     parser = argparse.ArgumentParser()
