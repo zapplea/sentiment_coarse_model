e85546680156b909b1c96d2a8425c493e59bc138
Yibing Liu <zapplea@gmail.com>
2018-08-16 21:33:15 +1000

    add

--- sentiment/functions/initializer/initializer.py
+++ sentiment/functions/initializer/initializer.py
@@ -8,7 +8,6 @@ class Initializer:
     #     return init
 
     def parameter_initializer(shape,dtype='float32'):
-        print('================initializer')
-        stdv=1/tf.sqrt(tf.constant(shape[-1],dtype=dtype))
-        init = tf.random_uniform(shape,minval=-stdv,maxval=stdv,dtype=dtype)
+        # stdv=1/tf.sqrt(tf.constant(shape[-1],dtype=dtype))
+        init = tf.random_uniform(shape,dtype=dtype)
         return init
