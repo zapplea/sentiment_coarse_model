17567caf1255521703115bf0a0ad8a52f39a80da
Yibing Liu <zapplea@gmail.com>
2018-09-05 02:38:28 +1000

    debug initializer

--- sentiment/functions/initializer/initializer.py
+++ sentiment/functions/initializer/initializer.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 
 class Initializer:
     @staticmethod
-    def parameter_initializer(shape,dtype='float32'):
-        stdv=1/tf.sqrt(tf.constant(shape[-1],dtype=dtype))
-        init = tf.random_uniform(shape,minval=-stdv,maxval=stdv,dtype=dtype,seed=1)
-        return init
-
     # def parameter_initializer(shape,dtype='float32'):
-    #     # stdv=1/tf.sqrt(tf.constant(shape[-1],dtype=dtype))
-    #     init = tf.random_normal(shape,stddev=3.0,dtype=dtype)
+    #     stdv=1/tf.sqrt(tf.constant(shape[-1],dtype=dtype))
+    #     init = tf.random_uniform(shape,minval=-stdv,maxval=stdv,dtype=dtype,seed=1)
     #     return init
+
+    def parameter_initializer(shape,dtype='float32'):
+        # stdv=1/tf.sqrt(tf.constant(shape[-1],dtype=dtype))
+        init = tf.random_normal(shape,stddev=1.0,dtype=dtype)
+        return init
