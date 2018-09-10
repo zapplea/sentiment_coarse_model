211b815cde57263f66effd10972382641c20e99b
Yibing Liu <zapplea@gmail.com>
2018-09-11 00:51:06 +1000

    add

--- sentiment/util/data_process/aic_process.py
+++ sentiment/util/data_process/aic_process.py
@@ -10,7 +10,7 @@ class AiC:
 
     def reader(self):
         data = pd.read_csv(self.config['train_filePath'])
-        print(data.iloc[:10][0])
+        print(type(data.iloc[:10]))
 
 
 if __name__ == "__main__":
