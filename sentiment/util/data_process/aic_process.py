21f248c871e8d299ca0fafa4ac5342cbdbc4214f
Yibing Liu <zapplea@gmail.com>
2018-09-11 01:04:50 +1000

    add

--- sentiment/util/data_process/aic_process.py
+++ sentiment/util/data_process/aic_process.py
@@ -10,11 +10,23 @@ class AiC:
 
     def reader(self):
         data = pd.read_csv(self.config['train_filePath'])
-        data = data.values
-        print(data[0])
+        train_data = data.values
+        print('train shape: ',train_data.shape)
+        data = pd.read_csv(self.config['testa_filePath'])
+        testa_data = data.values
+        print('test shape: ',testa_data.shape)
+        data = pd.read_csv(self.config['valid_filePath'])
+        valid_data = data.values
+        print('valid shape: ',valid_data.shape)
+
+
+    def split_sentence(self,sentence):
+
 
 
 if __name__ == "__main__":
-    config = {'train_filePath':'/datastore/liu121/sentidata2/expdata/aic2018/train/sentiment_analysis_trainingset.csv'}
+    config = {'train_filePath':'/datastore/liu121/sentidata2/expdata/aic2018/train/sentiment_analysis_trainingset.csv',
+              'testa_filePath':'/datastore/liu121/sentidata2/expdata/aic2018/testa/sentiment_analysis_testa.csv',
+              'valid_filePath':'/datastore/liu121/sentidata2/expdata/aic2018/valid/sentiment_analysis_validationset.csv',}
     aic = AiC(config)
     aic.reader()
