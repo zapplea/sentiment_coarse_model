a337aedc066a7d830bc438c43b04c71a5c059378
Yibing Liu <zapplea@gmail.com>
2018-09-14 02:28:58 +1000

    add: aic

---
+++ aic/functions/metrics.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import numpy as np
+
+class Metrics:
+    def TP(self,true_labels,pred_labels):
+        return np.count_nonzero(true_labels*pred_labels,axis=0).astype('float32')
+
+    def TN(self,true_labels,pred_labels):
+        return np.count_nonzero((pred_labels-1)*(true_labels-1),axis=0).astype('float32')
+
+    def FP(self,true_labels, pred_labels):
+        return np.count_nonzero(pred_labels*(true_labels-1), axis=0).astype('float32')
+
+    def FN(self,true_labels, pred_labels):
+        return np.count_nonzero((pred_labels-1)*true_labels,axis=0).astype('float32')
+
+    def precision(self, TP, FP, flag):
+        assert flag == 'macro' or flag == 'micro', 'Please enter right flag...'
+        if flag == 'macro':
+            tmp = np.nonzero((np.sum(TP, axis=0) + np.sum(FP, axis=0) == 0))
+            res = np.sum(TP, axis=0, dtype='float32') / (np.sum(TP, axis=0, dtype='float32') + np.sum(FP, axis=0, dtype='float32'))
+            res[tmp] = 1
+            return res
+        else:
+            return np.sum(TP) / (np.sum(TP) + np.sum(FP) + 1e-10)
+
+    def recall(self, TP, FN, flag):
+        assert flag == 'macro' or flag == 'micro', 'Please enter right flag...'
+        if flag == 'macro':
+            tmp = np.nonzero((np.sum(TP, axis=0) + np.sum(FN, axis=0) == 0))
+            res = np.sum(TP, axis=0, dtype='float32') / (
+            np.sum(TP, axis=0, dtype='float32') + np.sum(FN, axis=0, dtype='float32'))
+            res[tmp] = 1
+            return res
+        else:
+            return np.sum(TP) / (np.sum(TP) + np.sum(FN) + 1e-10)
+
+    def f1_score(self, precision, recall, flag):
+        assert flag == 'macro' or flag == 'micro', 'Please enter right flag...'
+        if flag == 'macro':
+            tmp = np.nonzero((precision + recall) == 0)
+            res = 2 * precision * recall / (precision + recall + 1e-10)
+            res[tmp] = 0
+            return res
+        else:
+            return 2 * precision * recall / (precision + recall + 1e-10)
---
+++ aic/learn/LEARN_TransferAttrNet.py
@@ -0,0 +1,42 @@
+import getpass
+import sys
+if getpass.getuser() == 'yibing':
+    sys.path.append('/home/yibing/Documents/csiro/sentiment_coarse_model')
+elif getpass.getuser() == 'lujunyu':
+    sys.path.append('/home/lujunyu/repository/sentiment_coarse_model')
+elif getpass.getuser() == 'liu121':
+    sys.path.append('/home/liu121/sentiment_coarse_model')
+
+import argparse
+
+from aic.coarse_net.attr_net import AttributeNet as CoarseAttributeNet
+from aic.fine_net.attr_net import AttributeNet as FineAttributeNet
+from aic.trains.transfer_atr_train import TransAtrTrain
+from aic.data_process.attr_datafeeder import DataFeeder
+
+def main(config):
+    datafeeder = DataFeeder(config)
+    train = TransAtrTrain(config, datafeeder)
+    fine_net = FineAttributeNet(config)
+    init_data = train.transfer(fine_net.classifier)
+    coarse_net = CoarseAttributeNet(config)
+    train.train(coarse_net.classifier,init_data)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num', type=int, default=0)
+    args = parser.parse_args()
+
+    reg_rate = [1E-5, ]
+    lr =       [1E-4, ]
+    config = {
+                'attribute_mat_size': 5,
+                'reg_rate': reg_rate[args.num],
+                'lr': lr[args.num],
+            }
+    config['tfb_filePath']='/datastore/liu121/sentidata2/resultdata/fine_nn/model/ckpt_reg%s_lr%s_mat%s' \
+                            % (str(reg_rate[args.num]), str(lr[args.num]), str(config['attribute_mat_size']))
+    config['report_filePath']='/datastore/liu121/sentidata2/resultdata/fine_nn/report/report_reg%s_lr%s_mat%s.info' \
+                              % (str(reg_rate[args.num]), str(lr[args.num]), str(config['attribute_mat_size']))
+
+    main(config)
