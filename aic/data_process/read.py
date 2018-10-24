73037a09b1867cbbaddd4f30bbc9953b3c909434
Yibing Liu <zapplea@gmail.com>
2018-10-24 12:06:12 +0800

    test

--- aic/data_process/read.py
+++ aic/data_process/read.py
@@ -8,3 +8,6 @@ with open('/datastore/liu121/sentidata2/expdata/aic2018/fine_data/dev_fine.pkl',
     sentence=np.array(sentence,dtype='int32')
     data=(data[0],data[1],sentence)
     print(data[0].shape,' ',data[1].shape,' ',data[2].shape)
+
+with open('/datastore/liu121/sentidata2/expdata/aic2018/fine_data/dev_fine.pkl','wb') as f:
+    pickle.dump(data,f)
