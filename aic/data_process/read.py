05aa60c3ac40e165b46c7148ca8ba272221e0ed7
Yibing Liu <zapplea@gmail.com>
2018-10-24 12:03:13 +0800

    test

--- aic/data_process/read.py
+++ aic/data_process/read.py
@@ -1,5 +1,10 @@
 import pickle
-
+import numpy as np
 with open('/datastore/liu121/sentidata2/expdata/aic2018/fine_data/dev_fine.pkl','rb') as f:
     data = pickle.load(f)
-    print(data[2].shape)
+    print(len(data))
+    sentence = []
+    for s in data[2]:
+        sentence.append(s)
+    sentence=np.array(sentence,dtype='int32')
+    print(sentence.shape)
