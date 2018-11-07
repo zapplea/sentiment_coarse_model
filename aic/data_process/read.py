87ba6dbc346e16ad20cdf1c1e8a32630c3f168a0
Yibing Liu <zapplea@gmail.com>
2018-11-08 06:07:04 +0800

    add

--- aic/data_process/read.py
+++ aic/data_process/read.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-with open('/datastore/liu121/sentidata2/expdata/aic2018/coarse_data/train_coarse.pkl','rb') as f:
+with open('/datastore/liu121/sentidata2/data/aic2018/coarse_data/train_coarse.pkl','rb') as f:
     attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
     print(senti_labels.shape)
 
