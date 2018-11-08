d489fd4721bb78c71bf975ad00eef2e8207156a6
Yibing Liu <zapplea@gmail.com>
2018-11-09 03:43:13 +0800

    add

--- aic/data_process/read.py
+++ aic/data_process/read.py
@@ -1,8 +1,8 @@
 import pickle
 import numpy as np
 
-with open('/datastore/liu121/sentidata2/data/aic2018/coarse_data_backup/train_coarse.pkl','rb') as f:
-    attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
+with open('/datastore/liu121/sentidata2/data/aic2018/coarse_data_backup/dev_coarse.pkl','rb') as f:
+    attr_labels, senti_labels, sentence = pickle.load(f)
     print(senti_labels.shape)
     print(attr_labels.shape)
     non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
@@ -19,8 +19,8 @@ with open('/datastore/liu121/sentidata2/data/aic2018/coarse_data_backup/train_co
     print(senti_labels.shape)
     print(senti_labels[0])
 
-with open('/datastore/liu121/sentidata2/data/aic2018/coarse_data/train_coarse.pkl','wb') as f:
-    pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
+with open('/datastore/liu121/sentidata2/data/aic2018/coarse_data/dev_coarse.pkl','wb') as f:
+    pickle.dump((attr_labels, senti_labels, sentence),f)
 
 # with open('/datastore/liu121/sentidata2/expdata/aic2018/coarse_data/dev_fine.pkl','wb') as f:
 #     pickle.dump(data,f)
