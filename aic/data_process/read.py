ea4253363d116a0b09c585f8b304631490e55a63
Yibing Liu <zapplea@gmail.com>
2018-11-13 02:04:31 +0800

    add

--- aic/data_process/read.py
+++ aic/data_process/read.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 
-top_k = None
+top_k = 500
 def train(infile,outfile):
     with open(infile,'rb') as f:
         attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
