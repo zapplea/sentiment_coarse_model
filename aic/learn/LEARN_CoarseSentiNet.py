7bcc7ac25ddbda1c5d8e3e47376a6b667e3a5ad6
Yibing Liu <zapplea@gmail.com>
2018-11-13 00:10:57 +0800

    add

--- aic/learn/LEARN_CoarseSentiNet.py
+++ aic/learn/LEARN_CoarseSentiNet.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
                 'attribute_mat_size': 5,
                 'reg_rate': reg_rate[args.num],
                 'lr': lr[args.num],
-                'batch_size':4,
+                'batch_size':20,
                 'gpu_num':2,
                 'epoch':10,
                 'attributes_num':20,
