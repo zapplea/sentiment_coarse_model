48aa5e18d07433bbeb3f01ede3a1f1991c7bf6dd
Yibing Liu <zapplea@gmail.com>
2018-11-14 02:46:18 +0800

    add

--- aic/learn/LEARN_CoarseSentiNet.py
+++ aic/learn/LEARN_CoarseSentiNet.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     config['train_data_file_path'] = '/datastore/liu121/sentidata2/data/aic2018/coarse_data/train_coarse.pkl'
     config['test_data_file_path'] = '/datastore/liu121/sentidata2/data/aic2018/coarse_data/dev_coarse.pkl'
 
-    config['sr_path'] = '/datastore/liu121/sentidata2/result/coarse_nn/ckpt_reg%s_lr%s_mat%s/' \
+    config['sr_path'] = '/datastore/liu121/sentidata2/result/coarse_nn/ckpt_reg%s_lr%s_mat%s/model.ckpt.meta' \
                         % (str(reg_rate[args.num]), str(lr[args.num]), str(config['attribute_mat_size']))
     config['report_filePath'] = '/datastore/liu121/sentidata2/report/coarse_nn/'
     main(config)
