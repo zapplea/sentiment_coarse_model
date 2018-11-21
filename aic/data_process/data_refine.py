9585eeb3d14c2478a31fe2561f511175f90641ad
Yibing Liu <zapplea@gmail.com>
2018-11-22 02:38:15 +0800

    add

--- aic/data_process/data_refine.py
+++ aic/data_process/data_refine.py
@@ -1,8 +1,9 @@
 import pickle
 import numpy as np
+import argparse
 
-top_k = 500
-def train(infile,outfile):
+
+def train(infile,outfile, top_k):
     print('train:')
     with open(infile,'rb') as f:
         attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
@@ -23,7 +24,7 @@ def train(infile,outfile):
         pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
     print('train successful\n')
 
-def test(infile,outfile):
+def test(infile,outfile, top_k):
     print('test:')
     with open(infile,'rb') as f:
         attr_labels, senti_labels, sentence = pickle.load(f)
@@ -45,6 +46,13 @@ def test(infile,outfile):
     print('test success')
 
 if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--topk',default=None)
+    parser.add_argument('--mod',default='coarse')
+    args = parser.parse_args()
+    top_k = args.topk
+    mod = args.mod
+
     path = {'coarse_train_in':'/datastore/liu121/sentidata2/data/aic2018/coarse_data_backup/train_coarse.pkl',
             'coarse_train_out':'/datastore/liu121/sentidata2/data/aic2018/coarse_data/train_coarse.pkl',
             'coarse_test_in':'/datastore/liu121/sentidata2/data/aic2018/coarse_data_backup/dev_coarse.pkl',
@@ -54,7 +62,9 @@ if __name__=='__main__':
             'fine_test_in':'/datastore/liu121/sentidata2/data/aic2018/fine_data_backup/dev_fine.pkl',
             'fine_test_out':'/datastore/liu121/sentidata2/data/aic2018/fine_data/dev_fine.pkl',
            }
-    # train(path['coarse_train_in'],path['coarse_train_out'])
-    # test(path['coarse_test_in'],path['coarse_test_out'])
-    train(path['coarse_train_in'],path['coarse_train_out'])
-    test(path['coarse_test_in'],path['coarse_test_out'])
+    if mod == 'coarse':
+        train(path['coarse_train_in'],path['coarse_train_out'],top_k)
+        test(path['coarse_test_in'],path['coarse_test_out'], top_k)
+    else:
+        train(path['fine_train_in'],path['fine_train_out'], top_k)
+        test(path['fine_test_in'],path['fine_test_out'], top_k)
