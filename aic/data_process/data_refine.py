ec9d4594a9aa5128cfc077b48dfe0121784535de
Yibing Liu <zapplea@gmail.com>
2018-11-23 04:29:35 +0800

    add

--- aic/data_process/data_refine.py
+++ aic/data_process/data_refine.py
@@ -10,7 +10,52 @@ def train(infile,outfile, top_k):
         print('shape of sentence: ',sentence.shape)
         print('shape of attributes: ', attr_labels.shape)
         print('shape of senti labels: ', senti_labels.shape)
+        
+        non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
+        non_attr_senti = np.tile(non_attr,reps=[1,3])
+        non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
+        senti_labels = np.concatenate([senti_labels,non_attr_senti],axis=1)
+        attr_labels=attr_labels[:top_k]
+        senti_labels=senti_labels[:top_k]
+        sentence=sentence[:top_k]
+        print('senti labels shape: ',senti_labels.shape)
+        print('attr labels shape: ',attr_labels.shape)
+        print('sentence shape: ',sentence.shape)
         return
+    with open(outfile,'wb') as f:
+        pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
+    print('train successful\n')
+
+def test(infile,outfile, top_k):
+    print('test:')
+    with open(infile,'rb') as f:
+        attr_labels, senti_labels, sentence = pickle.load(f)
+        print('shape of sentence: ', sentence.shape)
+        print('shape of attributes: ', attr_labels.shape)
+        print('shape of senti labels: ', senti_labels.shape)
+
+        non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
+        non_attr_senti = np.tile(non_attr,reps=[1,3])
+        non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
+        senti_labels = np.concatenate([senti_labels,non_attr_senti],axis=1)
+        attr_labels = attr_labels[:top_k]
+        senti_labels = senti_labels[:top_k]
+        sentence = sentence[:top_k]
+        print('senti labels shape: %s'%str(senti_labels.shape))
+        print('attr labels shape: %s'%str(attr_labels.shape))
+        print('sentence shape: %s'%str(sentence.shape))
+        return
+    with open(outfile,'wb') as f:
+        pickle.dump((attr_labels, senti_labels, sentence),f)
+    print('test success')
+
+def fine_train(infile,outfile, top_k):
+    print('train:')
+    with open(infile,'rb') as f:
+        attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
+        print('shape of sentence: ',sentence.shape)
+        print('shape of attributes: ', attr_labels.shape)
+        print('shape of senti labels: ', senti_labels.shape)
         non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
         non_attr_senti = np.tile(non_attr,reps=[1,3])
         non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
@@ -25,14 +70,13 @@ def train(infile,outfile, top_k):
         pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
     print('train successful\n')
 
-def test(infile,outfile, top_k):
+def fine_test(infile,outfile, top_k):
     print('test:')
     with open(infile,'rb') as f:
         attr_labels, senti_labels, sentence = pickle.load(f)
         print('shape of sentence: ', sentence.shape)
         print('shape of attributes: ', attr_labels.shape)
         print('shape of senti labels: ', senti_labels.shape)
-        return
         non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
         non_attr_senti = np.tile(non_attr,reps=[1,3])
         non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
