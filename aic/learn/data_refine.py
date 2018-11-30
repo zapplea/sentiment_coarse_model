c14667ed394a61aa04f7324e3ddcb3c2ff6a6818
Yibing Liu <zapplea@gmail.com>
2018-12-01 02:07:20 +0800

    add

--- aic/learn/data_refine.py
+++ aic/learn/data_refine.py
@@ -3,10 +3,14 @@ import numpy as np
 import argparse
 
 
-def train(infile,outfile, top_k):
-    print('train:')
+def arrange(infile,outfile, bottom, up, mod):
     with open(infile,'rb') as f:
-        attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
+        if mod == 'train':
+            print('train:')
+            attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
+        else:
+            print('test:')
+            attr_labels, senti_labels, sentence = pickle.load(f)
         print('shape of sentence: ',sentence.shape)
         print('shape of attributes: ', attr_labels.shape)
         print('shape of senti labels: ', senti_labels.shape)
@@ -15,81 +19,19 @@ def train(infile,outfile, top_k):
         non_attr_senti = np.tile(non_attr,reps=[1,3])
         non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
         senti_labels = np.concatenate([senti_labels,non_attr_senti],axis=1)
-        attr_labels=attr_labels[:top_k]
-        senti_labels=senti_labels[:top_k]
-        sentence=sentence[:top_k]
+        attr_labels=attr_labels[bottom:up]
+        senti_labels=senti_labels[bottom:up]
+        sentence=sentence[bottom:up]
         print('senti labels shape: ',senti_labels.shape)
         print('attr labels shape: ',attr_labels.shape)
         print('sentence shape: ',sentence.shape)
     with open(outfile,'wb') as f:
-        pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
-    print('train successful\n')
-
-def test(infile,outfile, top_k):
-    print('test:')
-    with open(infile,'rb') as f:
-        attr_labels, senti_labels, sentence = pickle.load(f)
-        print('shape of sentence: ', sentence.shape)
-        print('shape of attributes: ', attr_labels.shape)
-        print('shape of senti labels: ', senti_labels.shape)
-
-        non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
-        non_attr_senti = np.tile(non_attr,reps=[1,3])
-        non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
-        senti_labels = np.concatenate([senti_labels,non_attr_senti],axis=1)
-        attr_labels = attr_labels[:top_k]
-        senti_labels = senti_labels[:top_k]
-        sentence = sentence[:top_k]
-        print('senti labels shape: %s'%str(senti_labels.shape))
-        print('attr labels shape: %s'%str(attr_labels.shape))
-        print('sentence shape: %s'%str(sentence.shape))
-    with open(outfile,'wb') as f:
-        pickle.dump((attr_labels, senti_labels, sentence),f)
-    print('test success')
-
-def fine_train(infile,outfile, top_k):
-    print('train:')
-    with open(infile,'rb') as f:
-        attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed = pickle.load(f)
-        print('shape of sentence: ',sentence.shape)
-        print('shape of attributes: ', attr_labels.shape)
-        print('shape of senti labels: ', senti_labels.shape)
-        senti_labels = senti_labels[:,:,:-1]
-        non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
-        non_attr_senti = np.tile(non_attr,reps=[1,3])
-        non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
-        senti_labels = np.concatenate([senti_labels,non_attr_senti],axis=1)
-        attr_labels=attr_labels[:top_k]
-        senti_labels=senti_labels[:top_k]
-        sentence=sentence[:top_k]
-        print('senti labels shape: ',senti_labels.shape)
-        print('attr labels shape: ',attr_labels.shape)
-        print('sentence shape: ',sentence.shape)
-    with open(outfile,'wb') as f:
-        pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
-    print('train successful\n')
-
-def fine_test(infile,outfile, top_k):
-    print('test:')
-    with open(infile,'rb') as f:
-        attr_labels, senti_labels, sentence = pickle.load(f)
-        print('shape of sentence: ', sentence.shape)
-        print('shape of attributes: ', attr_labels.shape)
-        print('shape of senti labels: ', senti_labels.shape)
-        senti_labels = senti_labels[:, :, :-1]
-        non_attr = np.zeros((attr_labels.shape[0],1),dtype='float32')
-        non_attr_senti = np.tile(non_attr,reps=[1,3])
-        non_attr_senti = np.expand_dims(non_attr_senti,axis=1)
-        senti_labels = np.concatenate([senti_labels,non_attr_senti],axis=1)
-        attr_labels = attr_labels[:top_k]
-        senti_labels = senti_labels[:top_k]
-        sentence = sentence[:top_k]
-        print('senti labels shape: %s'%repr(senti_labels.shape))
-        print('attr labels shape: %s'%repr(attr_labels.shape))
-        print('sentence shape: %s'%repr(sentence.shape))
-    with open(outfile,'wb') as f:
-        pickle.dump((attr_labels, senti_labels, sentence),f)
-    print('test success')
+        if mod == 'train':
+            pickle.dump((attribute_dic, word_dic, attr_labels, senti_labels, sentence, word_embed),f)
+            print('train successful\n')
+        else:
+            pickle.dump((attr_labels, senti_labels, sentence),f)
+            print('test successful\n')
 
 def few_shot(infile,outfile, k_shot,mod):
     print('%s:'%mod)
@@ -139,13 +81,17 @@ def few_shot(infile,outfile, k_shot,mod):
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--trk',type=int,default=None)
-    parser.add_argument('--tek', type=int, default=None)
+    parser.add_argument('--trb',type=int,default=0)
+    parser.add_argument('--tru', type=int, default=None)
+    parser.add_argument('--teb', type=int, default=0)
+    parser.add_argument('--teu', type=int, default=None)
+
     parser.add_argument('--trf',type=int, default=float('inf'))
     parser.add_argument('--tef',type = int, default=float('inf'))
-    parser.add_argument('--mod',type=str,default='fine')
+
+    parser.add_argument('--cmod',type=str,default='coarse')
+    parser.add_argument('--fmod',type=str,default='few_shot')
     args = parser.parse_args()
-    mod = args.mod
 
     path = {'coarse_train_in':'/datastore/liu121/sentidata2/data/aic2018/coarse_data_backup/train_coarse.pkl',
             'coarse_train_out':'/datastore/liu121/sentidata2/data/aic2018/coarse_data/train_coarse.pkl',
@@ -156,15 +102,19 @@ if __name__=='__main__':
             'fine_test_in':'/datastore/liu121/sentidata2/data/aic2018/fine_data_backup/dev_fine.pkl',
             'fine_test_out':'/datastore/liu121/sentidata2/data/aic2018/fine_data/dev_fine.pkl',
            }
-    # if mod == 'coarse':
-    #     train(path['coarse_train_in'],path['coarse_train_out'],args.trk)
-    #     test(path['coarse_test_in'],path['coarse_test_out'], args.tek)
-    # else:
-    #     fine_train(path['fine_train_in'],path['fine_train_out'], args.trk)
-    #     fine_test(path['fine_test_in'],path['fine_test_out'], args.tek)
-    if mod == 'coarse':
-        few_shot(path['coarse_train_in'],path['coarse_train_out'],args.trf,mod='train')
-        few_shot(path['coarse_test_in'],path['coarse_test_out'], args.tef, mod='test')
+
+    if args.fmod == 'few_shot':
+        if args.cmod == 'coarse':
+            few_shot(path['coarse_train_in'], path['coarse_train_out'], args.trf, mod='train')
+            few_shot(path['coarse_test_in'], path['coarse_test_out'], args.tef, mod='test')
+        else:
+            few_shot(path['fine_train_in'], path['fine_train_out'], args.trf, mod='train')
+            few_shot(path['fine_test_in'], path['fine_test_out'], args.tef, mod='test')
     else:
-        few_shot(path['fine_train_in'], path['fine_train_out'], args.trf, mod='train')
-        few_shot(path['fine_test_in'], path['fine_test_out'], args.tef, mod='test')
+        if args.cmod == 'coarse':
+            arrange(path['coarse_train_in'],path['coarse_train_out'],args.trb,args.tru,mod='train')
+            arrange(path['coarse_test_in'],path['coarse_test_out'], args.teb, args.teu, mod='test')
+        else:
+            arrange(path['fine_train_in'],path['fine_train_out'], args.trb,args.tru,mod='train')
+            arrange(path['fine_test_in'],path['fine_test_out'], args.teb,args.teu,mod='test')
+
