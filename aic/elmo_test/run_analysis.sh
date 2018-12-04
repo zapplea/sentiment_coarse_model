0d3fc64659a64fc6f5d9df030599d4cb0b4eddb8
Yibing Liu <zapplea@gmail.com>
2018-12-05 02:53:57 +0800

    add

--- aic/elmo_test/run_analysis.sh
+++ aic/elmo_test/run_analysis.sh
@@ -8,4 +8,7 @@
 #SBATCH --cpus-per-task=1
 module load python/3.6.1
 
-python data_analysis.py
+python data_analysis.py \
+    --train_prefix='/datastore/liu121/sentidata2/data/bilm/training-monolingual.tokenized.shuffled/*' \
+    --vocab_file '/datastore/liu121/sentidata2/data/bilm/vocab.txt' \
+    --save_dir '/datastore/liu121/sentidata2/result/bilm/'
