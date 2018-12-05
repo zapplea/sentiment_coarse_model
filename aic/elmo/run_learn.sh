1628391f7ed21d292f1e6027f8ae13a697fb416f
Yibing Liu <zapplea@gmail.com>
2018-12-05 10:42:20 +0800

    add

--- aic/elmo/run_learn.sh
+++ aic/elmo/run_learn.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-#SBATCH --get-user-env
-#SBATCH --job-name="elmo train"
-#SBATCH --time=140:00:00
-#SBATCH --nodes=1
-#SBATCH --mem=200GB
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:2
-echo "loading"
-module load python/3.6.1
-module load tensorflow/1.6.0-py36-gpu
-echo "loaded"
+
 if [ $1 = 'cn' ]
 then
     python LEARN_elmo.py \
