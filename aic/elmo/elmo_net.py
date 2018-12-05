c163579f70c90c2ac793521d5863e50831680170
Yibing Liu <zapplea@gmail.com>
2018-12-06 00:11:09 +0800

    add

--- aic/elmo/elmo_net.py
+++ aic/elmo/elmo_net.py
@@ -528,14 +528,16 @@ class LanguageModel(object):
                     if self.is_training:
                         lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell,
                                                                   input_keep_prob=keep_prob)
+
+                # outputs.shape = (batch size, words num, lstm_dim)
+                outputs, _ = tf.nn.dynamic_rnn(lstm_cell, inputs=outputs,
+                                               dtype='float32', scope=scope_name)
+                print('outputs.shape: ', outputs.get_shape())
                 for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
                     print(var.name)
                     print(var.get_shape())
                 print('=====================')
-                print('outputs.shape: ', outputs.get_shape())
-                # outputs.shape = (batch size, words num, lstm_dim)
-                outputs, _ = tf.nn.dynamic_rnn(lstm_cell, inputs=outputs,
-                                               dtype='float32', scope=scope_name)
+            print('#################################')
 
             # (batch_size * unroll_steps, 512)
             lstm_output_flat = tf.reshape(
