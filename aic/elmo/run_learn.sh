9212f90c3d0c8c54e0b619d3df52610a1705e55f
Yibing Liu <zapplea@gmail.com>
2018-12-05 10:18:50 +0800

    add

--- aic/elmo/run_learn.sh
+++ aic/elmo/run_learn.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-python LEARN_elmo.py \
-    --train_prefix='/datastore/liu121/sentidata2/data/bilm/aic2018/*' \
-    --vocab_file '/datastore/liu121/sentidata2/data/bilm/vocab_aic.txt' \
-    --save_dir '/datastore/liu121/sentidata2/result/bilm/'
+if [ $1 = 'cn' ]
+then
+    python LEARN_elmo.py \
+        --train_prefix='/datastore/liu121/sentidata2/data/bilm/aic2018/*' \
+        --vocab_file '/datastore/liu121/sentidata2/data/bilm/vocab_aic.txt' \
+        --save_dir '/datastore/liu121/sentidata2/result/bilm/'
+elif [ $1 = 'en' ]
+then
+    python LEARN_elmo.py \
+        --train_prefix='/datastore/liu121/sentidata2/data/bilm/training-monolingual.tokenized.shuffled/*' \
+        --vocab_file '/datastore/liu121/sentidata2/data/bilm/vocab.txt' \
+        --save_dir '/datastore/liu121/sentidata2/result/bilm/'
+fi
