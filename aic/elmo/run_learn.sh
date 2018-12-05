3e0d79f3142a98614c2cf2613a544330305b15fe
Yibing Liu <zapplea@gmail.com>
2018-12-06 00:59:51 +0800

    add

--- aic/elmo/run_learn.sh
+++ aic/elmo/run_learn.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+#SBATCH --get-user-env
+#SBATCH --job-name="elmo"
+#SBATCH --time=140:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=200GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:2
 if [ $1 = 'cn' ]
 then
     python LEARN_elmo.py \
