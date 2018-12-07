ed7a2bef0616abedbc916b60464cb7c4eff82aa4
Yibing Liu <zapplea@gmail.com>
2018-12-07 17:29:15 +0800

    add

--- aic/trains/coarse_senti_train.py
+++ aic/trains/coarse_senti_train.py
@@ -71,34 +71,32 @@ class CoarseSentiTrain:
         print('epoch num: ',self.train_config['epoch'])
         for i in range(self.train_config['epoch']):
             dataset = self.dg.data_generator('train')
-            j = 0
             for attr_labels_data, senti_labels_data, sentences_data in dataset:
                 data_dict = {'X_data':sentences_data,'Y_att_data':attr_labels_data,
                              'Y_senti_data':senti_labels_data,'keep_prob':self.train_config['keep_prob_lstm']}
                 feed_dict = self.generate_feed_dict(graph=graph,gpu_num=gpu_num,data_dict=data_dict)
                 _, attr_train_loss, senti_train_loss, attr_pred_data, senti_pred_data \
                     = sess.run([train_step, attr_loss, senti_loss, attr_pred, senti_pred],feed_dict=feed_dict)
-                if dic['test_mod'] != 'attr':
-                    senti_score_with_inf, senti_score, senti_W, attended_senti_W, item1, A_Vi, item2 =sess.run([tf.get_collection('senti_score_with_inf')[0],
-                                           tf.get_collection('senti_score')[0],
-                                           tf.get_collection('senti_W')[0],
-                                           tf.get_collection('attended_senti_W')[0],
-                                           tf.get_collection('item1')[0],
-                                           tf.get_collection('A_Vi')[0],
-                                           tf.get_collection('item2')[0],],
-                                         feed_dict=feed_dict)
-                    senti_coarse_W = sess.run(graph.get_collection('senti_coarse_W'))
-                    self.mt.report('========================',self.analf,'analysis')
-                    self.mt.report('%s batch No.: %d\n'%(dic['test_mod'],j),self.analf,'analysis')
-                    self.mt.report('senti_W:\n%s'%str(senti_W),self.analf,'analysis')
-                    self.mt.report('attended_senti_W:\n%s' % str(attended_senti_W),self.analf,'analysis')
-                    self.mt.report('item1:\n%s'%str(item1),self.analf,'analysis')
-                    self.mt.report('A_Vi:\n%s' % str(A_Vi), self.analf, 'analysis')
-                    self.mt.report('item2:\n%s' % str(item2), self.analf, 'analysis')
-                    self.mt.report('senti_score_with_inf:\n%s' % str(senti_score_with_inf), self.analf, 'analysis')
-                    self.mt.report('senti_score:\n%s' % str(senti_score), self.analf, 'analysis')
-                    self.mt.report('senti_coarse_W:\n%s'%str(senti_coarse_W),self.analf,'analysis')
-                    j+=1
+            if dic['test_mod'] != 'attr':
+                senti_score_with_inf, senti_score, senti_W, attended_senti_W, item1, A_Vi, item2 =sess.run([tf.get_collection('senti_score_with_inf')[0],
+                                       tf.get_collection('senti_score')[0],
+                                       tf.get_collection('senti_W')[0],
+                                       tf.get_collection('attended_senti_W')[0],
+                                       tf.get_collection('item1')[0],
+                                       tf.get_collection('A_Vi')[0],
+                                       tf.get_collection('item2')[0],],
+                                     feed_dict=feed_dict)
+                senti_coarse_W = sess.run(graph.get_collection('senti_coarse_W'))
+                self.mt.report('========================',self.analf,'analysis')
+                self.mt.report('%s epoch: %d\n'%(dic['test_mod'],i),self.analf,'analysis')
+                self.mt.report('senti_W:\n%s'%str(senti_W),self.analf,'analysis')
+                self.mt.report('attended_senti_W:\n%s' % str(attended_senti_W),self.analf,'analysis')
+                self.mt.report('item1:\n%s'%str(item1),self.analf,'analysis')
+                self.mt.report('A_Vi:\n%s' % str(A_Vi), self.analf, 'analysis')
+                self.mt.report('item2:\n%s' % str(item2), self.analf, 'analysis')
+                self.mt.report('senti_score_with_inf:\n%s' % str(senti_score_with_inf), self.analf, 'analysis')
+                self.mt.report('senti_score:\n%s' % str(senti_score), self.analf, 'analysis')
+                self.mt.report('senti_coarse_W:\n%s'%str(senti_coarse_W),self.analf,'analysis')
 
 
             if i % self.train_config['epoch_mod'] == 0:
