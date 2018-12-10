876101371e2135b5a3b697452c3afef832f9db73
Yibing Liu <zapplea@gmail.com>
2018-12-10 11:17:07 +0800

    add

--- aic/sbatch/run_CoarseSentiNet.sh
+++ aic/sbatch/run_CoarseSentiNet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --get-user-env
 #SBATCH --job-name="sentiment net"
-#SBATCH --time=12:00:00
+#SBATCH --time=30:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=50GB
+#SBATCH --mem=60GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:2
