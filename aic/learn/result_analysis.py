8e5faf75d6cc697c61e817c6854f1a63b3b94943
Yibing Liu <zapplea@gmail.com>
2018-12-13 03:06:24 +0800

    add

--- aic/learn/result_analysis.py
+++ aic/learn/result_analysis.py
@@ -22,6 +22,7 @@ def analysis(dic,key,outfile):
     # TODO: whether the nan is caused by non-attribute.
     # TODO: check each operation, to see whether they get the expected value.
     # TODO: whehter it is caused by softmax loss when all label is 0
+    # TODO: two ways to check 1. change attr pred labe to Y_att in joint 2. eliminate step2, only preserve step1 and 3.
 
 if __name__ == "__main__":
     dic = load('/home/yibing/report/12.11/analysis_reg1e-05_lr0.001_mat5.info')
