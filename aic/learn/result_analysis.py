4c3aebf056f62156f4fc804d87c7b40cfd3d5da5
Yibing Liu <zapplea@gmail.com>
2018-12-16 23:22:52 +0800

    add

--- aic/learn/result_analysis.py
+++ aic/learn/result_analysis.py
@@ -1,8 +1,8 @@
 import pickle
 
-def load(filePath):
+def new_pickle(in_filePath,out_filePath):
     dic = {}
-    with open(filePath,'rb') as f:
+    with open(in_filePath,'rb') as f:
         while True:
             try:
                 data = pickle.load(f)
@@ -10,21 +10,34 @@ def load(filePath):
                     dic[key]=data[key]
             except Exception:
                 break
-    return dic
+    with open(out_filePath,'wb') as f:
+        pickle.dump(dic,f)
+
+def load(filePath):
+    with open(filePath,'rb') as f:
+        data = pickle.load(f)
+        return data
 
 def write(outfile,info):
     outfile.write(str(info))
 
-def analysis(dic,key,outfile):
-    data = dic[key]
-    for key in data:
-        write(outfile,key+':\n'+str(data[key])+'\n')
+def analysis(dic,key,out_filePath):
+    with open(out_filePath,'w+') as outfile:
+        data = dic[key]
+        for key in data:
+            write(outfile,key+':\n'+str(data[key])+'\n')
     # TODO: whether the nan is caused by non-attribute.
     # TODO: check each operation, to see whether they get the expected value.
     # TODO: whehter it is caused by softmax loss when all label is 0
     # TODO: two ways to check 1. change attr pred labe to Y_att in joint 2. eliminate step2, only preserve step1 and 3.
 
 if __name__ == "__main__":
-    dic = load('/home/yibing/report/12.11/analysis_reg1e-05_lr0.001_mat5.info')
-    with open('/home/yibing/report/12.11/result.txt','w+') as f:
-        analysis(dic,'%s epoch: %d'%('senti',0),f)
+    newpkl_filePath = '/datastore/liu121/sentidata2/report/coarse_nn/newpkl_reg1e-05_lr0.001_mat5.info'
+    anal_filePath = '/datastore/liu121/sentidata2/report/coarse_nn/analysis_reg1e-05_lr0.001_mat5.info'
+    new_pickle(anal_filePath,newpkl_filePath)
+    # dic = load(newpkl_filePath)
+    # s = '%s epoch: %d'
+    # keys = [s%('joint',0)]
+    # for key in keys:
+    #     out_filePath ='/datastore/liu121/sentidata2/report/coarse_nn/result(%s).txt'%key
+    #     analysis(dic,key,out_filePath)
