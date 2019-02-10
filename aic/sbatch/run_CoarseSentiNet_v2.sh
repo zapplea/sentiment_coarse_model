3765d1e4e2d4039ae8d03160d3a5926bd3b3cc14
yibing <zapplea@gmail.com>
2019-02-10 19:38:04 +0800

    add

--- aic/sbatch/run_CoarseSentiNet_v2.sh
+++ aic/sbatch/run_CoarseSentiNet_v2.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-python ../learn/data_refine.py --fmod arrange --tru 500 --teu 500
+#SBATCH --get-user-env
+#SBATCH --job-name="v2_coarse_nn"
+#SBATCH --time=140:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=60GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:2
+echo "loading"
+module load python/3.6.1
+module load tensorflow/1.6.0-py36-gpu
+echo "loaded"
+
+python ../learn/LEARN_CoarseSentiNet.py --num $1 --epoch $2
---
+++ aic/sbatch/run_dataRefine.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ../learn/data_refine.py --fmod arrange --tru 500 --teu 500
